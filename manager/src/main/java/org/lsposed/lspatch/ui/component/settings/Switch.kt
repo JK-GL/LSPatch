ff1b69145168524c433392c8bc8ac4fdbe3226b0
JK-GL <112629365+JK-GL@users.noreply.github.com>
2026-05-01 13:53:36 +0800

    revert: restore original SettingsSwitch.kt

--- manager/src/main/java/org/lsposed/lspatch/ui/component/settings/Switch.kt
+++ manager/src/main/java/org/lsposed/lspatch/ui/component/settings/Switch.kt
@@ -6,13 +6,10 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Api
 import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import org.lsposed.lspatch.ui.theme.XMColors
 
 @Composable
 fun SettingsSwitch(
@@ -25,24 +22,28 @@ fun SettingsSwitch(
     extraContent: (@Composable ColumnScope.() -> Unit)? = null
 ) {
     SettingsSlot(modifier, enabled, icon, title, desc, extraContent) {
-        Switch(
-            checked = checked,
-            onCheckedChange = null,
-            colors = SwitchDefaults.colors(
-                checkedThumbColor = Color.White,
-                checkedTrackColor = XMColors.accent,
-                uncheckedThumbColor = Color.White,
-                uncheckedTrackColor = XMColors.textSecondary.copy(alpha = 0.3f)
-            )
-        )
+        Switch(checked = checked, onCheckedChange = null)
     }
 }
 
 @Preview
 @Composable
-private fun Preview() {
-    var c by remember { mutableStateOf(false) }
+private fun SettingsCheckBoxPreview() {
+    var checked1 by remember { mutableStateOf(false) }
+    var checked2 by remember { mutableStateOf(false) }
     Column {
-        SettingsSwitch(modifier = Modifier.clickable { c = !c }, checked = c, title = "Title", desc = "Desc")
+        SettingsSwitch(
+            modifier = Modifier.clickable { checked1 = !checked1 },
+            checked = checked1,
+            title = "Title",
+            desc = "Description"
+        )
+        SettingsSwitch(
+            modifier = Modifier.clickable { checked2 = !checked2 },
+            checked = checked2,
+            icon = Icons.Outlined.Api,
+            title = "Title",
+            desc = "Description"
+        )
     }
 }

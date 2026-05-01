92b8943529dc532c5481243dd858b7508aea73fd
JK-GL <112629365+JK-GL@users.noreply.github.com>
2026-05-01 14:35:00 +0800

    fix: restore correct Theme.kt from JingMatrix original

--- manager/src/main/java/org/lsposed/lspatch/ui/theme/Theme.kt
+++ manager/src/main/java/org/lsposed/lspatch/ui/theme/Theme.kt
@@ -1,14 +1,16 @@
 package org.lsposed.lspatch.ui.theme
+
 import android.app.Activity
 import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
+
 @Composable
 fun LSPTheme(
     isDarkTheme: Boolean = isSystemInDarkTheme(),
@@ -30,6 +32,7 @@ fun LSPTheme(
             ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = !isDarkTheme
         }
     }
+
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
